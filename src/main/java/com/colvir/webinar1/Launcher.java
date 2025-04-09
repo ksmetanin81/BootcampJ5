08ff1013ad6dee9d4e0149d29cc902e6ffc3b68f
Konstantin <kotsa81@mail.ru>
2025-04-09 16:48:30 +0300

    First webinar

---
+++ src/main/java/com/colvir/webinar1/Launcher.java
@@ -0,0 +1,14 @@
+package com.colvir.webinar1;
+
+public class Launcher {
+
+    public static void main(String[] args){
+        int digit = 100;
+        double value = 20.0;
+        boolean flag = true;
+
+        String text = "Hello World!";
+        System.out.println(text);
+    }
+
+}
