658f37bcaa98f2f0da5a7942fd06e9d1686fce61
Konstantin <kotsa81@mail.ru>
2025-04-10 18:42:07 +0300

    Homework 1. Sorting strings using StreamAPI. Correction of comments

--- src/main/java/com/colvir/homework1_streamAPI/Launcher.java
+++ src/main/java/com/colvir/homework1_streamAPI/Launcher.java
@@ -1,24 +1,26 @@
 package com.colvir.homework1_streamAPI;
 
-import java.util.Arrays;
-import java.util.Scanner;
+import java.util.*;
+import java.util.function.Function;
 import java.util.stream.Collectors;
 
 public class Launcher {
 
-    public static void main(String[] args){
+    public static void main(String[] args) {
         System.out.println("Enter text:");
         Scanner console = new Scanner(System.in);
         String text = console.nextLine();
-
-        Arrays.stream(text.split(" "))
-                .map(e -> e.replaceAll("[^A-Za-zА-Яа-я0-9]", "").toLowerCase())
-                .collect(Collectors.toMap(key->key,value->1, Integer::sum))
-                .entrySet().stream()
-                .sorted((e1, e2) -> {
-                    return e1.getValue().compareTo(e2.getValue()) * -1;
-                })
-                .forEach(e -> System.out.println(e.getKey()));
-     }
+        if (!text.isBlank()) {
+            Arrays.stream(text.split(" "))
+                    .map(e -> e.replaceAll("[^A-Za-zА-Яа-я0-9]", "").toLowerCase())
+                    .collect(Collectors.groupingBy(Function.identity(), LinkedHashMap::new, Collectors.counting()))
+                    .entrySet().stream()
+                    .sorted(Map.Entry.<String, Long>comparingByValue().reversed())
+                    .map(Map.Entry::getKey)
+                    .forEach(System.out::println);
+        } else {
+            System.out.println("the text is empty");
+        }
+    }
 
 }
