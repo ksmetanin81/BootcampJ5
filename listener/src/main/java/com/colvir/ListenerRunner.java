bb71d2c1048ddf30ebd379028be6e4acda19f246
Khovala81 <kotsa81@mail.ru>
2025-06-09 21:30:23 +0300

    homework. Message brokers

---
+++ listener/src/main/java/com/colvir/ListenerRunner.java
@@ -0,0 +1,12 @@
+package com.colvir;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ListenerRunner {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ListenerRunner.class, args);
+    }
+}
